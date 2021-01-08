4d52e9c330ae130b699f15a66027193774fc5751
Edozie <phynnedo@gmail.com>
2021-01-08 09:49:54 +0100

    made colour adjustments and set responsiveness

--- script/index.js
+++ script/index.js
@@ -2,9 +2,3 @@
 document.querySelector('.header__links--1').addEventListener('click', function(){
     prompt("Subscribe to our email newsletter and get helpful tips on hair care and skin care by putting in your email address.")
 })
-
-/*document.querySelector('#checkbox').addEventListener('click', openSidebar)
-
-function openSidebar(){
-    document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
-}*/
